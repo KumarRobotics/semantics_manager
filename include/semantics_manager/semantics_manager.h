df1869a8e82869b12633a8712565ffd459b8277b
Ian D. Miller <iandouglas96@gmail.com>
2022-09-28 12:18:59 -0400

    Added loader library for config fiels

--- include/semantics_manager/semantics_manager.h
+++ include/semantics_manager/semantics_manager.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "semantics_manager/semantic_color_lut.h"
+#include <Eigen/Dense>
+
 namespace semantics_manager {
 
 static std::string getMapPath(const std::string& config_path);
@@ -8,22 +11,23 @@ static std::string getClassesPath(const std::string& config_path);
 struct ClassConfig {
   int num_classes;
   SemanticColorLut color_lut;
-  Eigen::VectorXi class_to_flattened;
-  Eigen::VectorXi flattened_to_class;
-  Eigen::VectorXi exclusivity;
-  Eigen::VectorXi traversabililty_diff;
-  Eigen::VectorXf loc_weight;
+  std::vector<int> class_to_flattened;
+  std::vector<int> flattened_to_class;
+  std::vector<int> exclusivity;
+  std::vector<int> traversabililty_diff;
+  std::vector<float> loc_weight;
 
   ClassConfig(const std::string& class_config_path);
-}
+};
 
 struct MapConfig {
-  std::string svg_path;
-  std::string raster_path;
-  std::string viz_path;
+  bool dynamic;
+  std::string svg_path{""};
+  std::string raster_path{""};
+  std::string viz_path{""};
   float resolution;
 
   MapConfig(const std::string& map_config_path);
-}
+};
 
 } // namespace semantics_manager
--- src/semantics_manager.cpp
+++ src/semantics_manager.cpp
@@ -5,62 +5,86 @@
 
 namespace semantics_manager {
 
-std::string SemanticsManager::getMapPath(const std::string& config_path) {
+std::string getMapPath(const std::string& config_path) {
   using namespace std::filesystem;
-  const YAML::Node config_file = YAML::LoadFile(world_config_path);
-  return (path(world_config_path).parent_path() / 
-          path(config_file["map"].as<std::string>())).string();
+  const YAML::Node config_file = YAML::LoadFile(config_path);
+  return path(config_path).parent_path() / 
+         path(config_file["map"].as<std::string>());
 }
 
-std::string SemanticsManager::getClassesPath(const std::string& config_path) {
+std::string getClassesPath(const std::string& config_path) {
   using namespace std::filesystem;
-  const YAML::Node config_file = YAML::LoadFile(world_config_path);
-  return (path(world_config_path).parent_path() / 
-          path(config_file["classes"].as<std::string>())).string();
+  const YAML::Node config_file = YAML::LoadFile(config_path);
+  return path(config_path).parent_path() / 
+         path(config_file["classes"].as<std::string>());
 }
 
 ClassConfig::ClassConfig(const std::string& class_config_path) {
   const YAML::Node class_config_file = YAML::LoadFile(class_config_path);
-  num_classes = class_config_file.
+  num_classes = class_config_file.size();
 
   // Initialize things
-  flatten_lut_ = Eigen::VectorXi::Zero(256);
+  exclusivity.resize(num_classes);
+  traversabililty_diff.resize(num_classes);
+  loc_weight.resize(num_classes);
 
   // First pass to build class lookup dict
-  int map_class_ind = 0;
   std::map<std::string, int> class_name_map;
+  int flattened_class_ind = 0;
   for (const auto& map_class : class_config_file) {
     // Classes not remapped are the ones we are going to end up with
     if (!map_class["remap"]) {
-      class_name_map.emplace(map_class["name"].as<std::string>(), map_class_ind);
-      if (map_class["exclusive"].as<bool>()) {
-        map_params.exclusive_classes.push_back(map_class_ind);
-      }
-      ++map_class_ind;
+      class_name_map.emplace(map_class["name"].as<std::string>(), flattened_class_ind);
+      ++flattened_class_ind;
     }
   }
-  map_params.num_classes = map_class_ind;
 
-  map_class_ind = 0;
+  class_to_flattened.resize(num_classes);
+  flattened_to_class.resize(flattened_class_ind);
+
+  int map_class_ind = 0;
+  flattened_class_ind = 0;
   for (const auto& map_class : class_config_file) {
-    auto color = map_class["color"];
+    exclusivity[map_class_ind] = map_class["exclusive"].as<bool>();
+    traversabililty_diff[map_class_ind] = map_class["traversabililty_diff"].as<int>();    
+    loc_weight[map_class_ind] = map_class["loc_weight"].as<float>();
+
+    // Manage loading the remappings
     if (map_class["remap"]) {
       // remap stuff
       auto remap_class = class_name_map.find(map_class["remap"].as<std::string>());
       if (remap_class != class_name_map.end()) {
-        flatten_lut_[map_class_ind] = remap_class->second + 1;
+        class_to_flattened[map_class_ind] = remap_class->second;
       }
     } else {
-      flatten_lut_[map_class_ind] = map_class_ind + 1;
+      class_to_flattened[map_class_ind] = flattened_class_ind;
+      flattened_to_class[flattened_class_ind] = map_class_ind;
+      ++flattened_class_ind;
     }
     ++map_class_ind;
   }
 
-  map_params.color_lut = SemanticColorLut(map_params.class_path);
-  nh_.param<float>("out_of_bounds_const", map_params.out_of_bounds_const, 5);
+  color_lut = SemanticColorLut(class_config_path);
 }
 
 MapConfig::MapConfig(const std::string& map_config_path) {
+  const YAML::Node map_config_file = YAML::LoadFile(map_config_path);
+  using namespace std::filesystem;
+  auto parent_path = path(map_config_path).parent_path();
+
+  resolution = map_config_file["resolution"].as<float>();
+  dynamic = map_config_file["dynamic"].as<bool>();
+  if (!dynamic) {
+    if (map_config_file["svg"]) {
+      svg_path = parent_path / path(map_config_file["svg"].as<std::string>());
+    }
+    if (map_config_file["raster"]) {
+      raster_path = parent_path / path(map_config_file["raster"].as<std::string>());
+    }
+    if (map_config_file["viz"]) {
+      viz_path = parent_path / path(map_config_file["viz"].as<std::string>());
+    }
+  }
 }
 
 } // namespace semantics_manager
